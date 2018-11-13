2356f3f307bd07985e427aaad7ee626e73134f27
Anton Flodin <anton.flodin@knowit.se>
2018-11-13 16:02:08 +0100

    Added weighted feature.

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,11 +14,13 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = isWeighted;
         }
 
         public void AddStudent(Student student)
@@ -77,6 +79,8 @@ namespace GradeBook.GradeBooks
 
         public static BaseGradeBook Load(string name)
         {
+            IsWeighted = isWeighted
+
             if (!File.Exists(name + ".gdbk"))
             {
                 Console.WriteLine("Gradebook could not be found.");
@@ -107,20 +111,33 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            int gpa = 0;
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    gpa = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    gpa = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    gpa = 2;
+                    break;
                 case 'D':
-                    return 1;
+                    gpa = 1;
+                    break;
                 case 'F':
-                    return 0;
+                    gpa = 0;
+                    break;
             }
-            return 0;
+
+            if (IsWeighted)
+            {
+                gpa++;
+            }
+                 
+
+            return gpa;
         }
 
         public virtual void CalculateStatistics()
